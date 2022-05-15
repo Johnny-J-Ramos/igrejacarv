b583c5d9ac688587ba4409731bb004792abd10f0
Johnny Jefferson José Ramos <61524749+Johnny-J-Ramos@users.noreply.github.com>
2022-05-14 22:19:15 -0300

    refatorando código do site da CARV

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
+const path = require('path');
+const connetToDb = require('./src/database/db');
+
+const port = process.env.PORT || 3333;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+connetToDb();
 
 app.get('/', (req, res) => {
-   return res.json({
-      name: "Johnny"
-   })
+   res.send('hello word')
 })
 
 app.listen(3333, () =>
-  console.log('Servidor conectador!')
+   console.log(`Servidor rodando em http://localhost:${port}`)
 );
